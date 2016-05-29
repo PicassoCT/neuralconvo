5ff34cc34085a6205adb2e8cf534c495cae1072e
Pica <pica@pica-R530-R730-R540>
2016-05-29 18:18:22 +0200

    changes

--- spring_chat_allchannels.lua
+++ spring_chat_allchannels.lua
@@ -2,86 +2,90 @@
 	local stringx = require "pl.stringx"
 	local xlua = require "xlua"
 
-	local function parsedLines(file, fields)
-		local f = assert(io.open(file, 'r'))
-		
-		return function()
-			local line = f:read("*line")
-			
-			if line == nil then
-				f:close()
-				return
-			end
-			
-			local values = stringx.split(line, " ***+*** ")
-			local t = {}
-			
-			for i,field in ipairs(fields) do
-				t[field] = values[i]
-			end
-			
-			return t
-		end
-	end
-
-	function trim6(s)
+	function trim(s)
 		if not s then return end
 		return s:match'^()%s*$' and '' or s:match'^%s*(.*%S)'
 	end
 
 	AllCharacters={}
+	characterCounter=0
 
 	local function extractMetaLineFromLog(file)
 		local f = assert(io.open(file, 'r'))
-		Lines={}
-		LineNr=0
+
+		 Lines={}
+		LineNr=1
+		Sstart, Send=0,0
 		
-		for line in fp:lines() do
-			character,Send= trim(string.sub(string.find(line,"<"),string.find(line,">")))
-			character2ID = "anyone"
+		for line in f:lines() do
+		
+			Sstart=string.find(line,"<")
+			Send =string.find(line,">")
+			Lines[LineNr]={}
 			
-			if character == "<Nightwatch>" then 
-				Sstart, Send= string.find("<Nightwatch>")
-				line= string.sub(line,Send)
-				character= trim(string.sub(string.find(line,"<"),string.find(line,">")))
-			end
+			if  Sstart and Send then 
+				Lines[LineNr].boolValidLine=true
+				character= trim(string.sub(line,Sstart,Send))
+				character2ID = "anyone"
+				
+				
+				if character == "<Nightwatch>" then 
+					character= nil
+					Sstart, Send= string.find(line,"<Nightwatch>")
+					line= string.sub(line,string.len(line))
+					
+					Sstart, Send= string.find(line,"<"),string.find(line,">")
+					if Sstart and Send then
+						character= trim(string.sub(line,Sstart,Send))
+					end
+				end
 			
-			if character then
-				--remove the braces	
-				character=trim(string.sub(character,1,string.len(character)-2))
-				
-				if not AllCharacters[character] then AllCharacters[character]= character end
-				
-				searchString=string.sub(line,Send,string.len(line))
-				
-				for dramatispersona,_ in pairs(AllCharacters) do
-					if dramatispersona ~= character then
-						if string.find(searchString,dramatispersona) then
-							character2ID= dramatispersona		
-						end
+				if character then
+					--remove the braces	
+					character=string.upper(trim(string.sub(character,2,string.len(character)-1)))
+					
+					if not AllCharacters[character] then 
+						print("New Person:"..character)
+						AllCharacters[character]= character 
 					end
 					
+					local searchString=string.sub(line,Send+1,string.len(line)-(Send+1))
+
+					for dramatispersona,_ in pairs(AllCharacters) do
+						if string.lower(dramatispersona) ~= string.lower(character) then
+							if string.find(searchString,dramatispersona) then
+								character2ID= string.upper(dramatispersona)		
+							end
+						end
+						
+					end
+					
+					
+						Lines[LineNr]= {		
+							characterID=character, 
+							character2ID = character2ID,
+							text = searchString,
+							lineID= LineNr
+						}
+					
+						if Lines[LineNr].character2ID =="anyone" then
+						
+							if Lines[LineNr-1] and Lines[LineNr-1].characterID	then	
+							Lines[LineNr].character2ID = Lines[LineNr-1].characterID
+							else
+							Lines[LineNr].character2ID=Lines[LineNr].characterID
+							end
+						end
 				end
-				
-				
-				Lines[LineNr]= {		character=character, 
-					character2ID = character2ID,
-					text = searchString,
-					lineID= LineNr,
-					boolnewConversation= false
-				}
-				
-				if Lines[LineNr-1]	then	
-					Lines[LineNr-1].nextLine= LineNr
-				end
-				
-				
-			else
-				Lines[LineNr]={ boolnewConversation=true
-				}
+				 	
+			end 
+		
+			if not Sstart or not Ssend then
+				Lines[LineNr].boolValidLine=false	
 			end
 			
-			LineNr=LineNr+1
+				
+		LineNr=LineNr+1
 		end 
 		f:close() 
 		
@@ -108,19 +112,27 @@
 		local count = 0
 		
 		print("-- Parsing Chat Dialog data set ...")
+		path= self.dir .. "/dataset.txt"
+		print(path)
 		
-		for line in extractMetaLineFromLog(self.dir .. "/dataset.txt") do
-			lines[line.lineID] = line
-		end
+		lines =extractMetaLineFromLog(path)
 		
-		conversation = {}
-		for line in lines do
-			if line.boolnewConversation == true then
+		local conversation = {}
+		lineAddedCounter=0
+		conversationID=0
+		
+		for index, line in ipairs(lines) do
+			if line.boolValidLine == false  and lineAddedCounter > 0 then
+				conversation.conversationID=conversationID
 				table.insert(conversations, conversation)
 				conversation = {}
+				conversationID=conversationID+1
+				lineAddedCounter= 0
 			end
-			table.insert(conversation, line)	
 			
+			
+			table.insert(conversation, line)	
+			lineAddedCounter=lineAddedCounter+1
 			count = count + 1
 			progress(count)
 		end
